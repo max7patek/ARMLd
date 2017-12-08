9f72067e8b79e306b8577fa153fdd103b323b028
Maxwell Patek <max7patek@gmail.com>
2017-12-08 10:14:07 -0500

    First commit. OpenAI Gym working. Atari Pong running.

--- src/Main.py
+++ src/Main.py
@@ -1,4 +1,39 @@
 import tensorflow as tf
+import gym
 
 print("Hello TensorFlow")
 
+env = gym.make('Pong-v0')
+
+env.reset()
+env.render()
+
+print(env.action_space)
+
+while True:
+    env.step(2)
+    env.render()
+
+
+
+'''
+ACTION_MEANING = {
+    0 : "NOOP",
+    1 : "FIRE",
+    2 : "UP",
+    3 : "RIGHT",
+    4 : "LEFT",
+    5 : "DOWN",
+    6 : "UPRIGHT",
+    7 : "UPLEFT",
+    8 : "DOWNRIGHT",
+    9 : "DOWNLEFT",
+    10 : "UPFIRE",
+    11 : "RIGHTFIRE",
+    12 : "LEFTFIRE",
+    13 : "DOWNFIRE",
+    14 : "UPRIGHTFIRE",
+    15 : "UPLEFTFIRE",
+    16 : "DOWNRIGHTFIRE",
+    17 : "DOWNLEFTFIRE",
+}'''
