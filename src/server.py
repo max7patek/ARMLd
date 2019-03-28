6f70e40c2c61c921dc020e6ae9d20fa77ec30c79
Maxwel Patek (vm) <max7patek@gmail.com>
2019-03-28 19:07:19 -0400

    added drone connection logic

--- src/server.py
+++ src/server.py
@@ -1,10 +1,32 @@
 from flask import Flask
+import tellopy
+
 app = Flask(__name__)
+
+speed = 100
+landed = True
+drone = None
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    global landed
+    global drone
+    if drone is None:
+        print("connecting to drone")
+        drone = tellopy.Tello()
+        drone.connect()
+    if landed:
+        drone.takeoff()
+        ret = "Taking Off"
+    else:
+        drone.land()
+        ret = "Landing"
+    landed = not landed
+    return ret
+    #return "9000?"
+
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=True)
+
